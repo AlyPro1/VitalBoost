ae1b017f40b84795532271be1899d68007787eb2
Aly <alyasghar77@gmail.com>
2025-09-13 16:42:15 +0500

    Updated quit-smoking-modal.js

--- quit-smoking-modal.js
+++ quit-smoking-modal.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Initialize modal animations and interactions
     function initializeModalAnimations() {
-  startSmokePuffs(); randomizeButterflies(); enableModalFocusTrap(quitModal);
+ 
         // Initialize calculator functionality
         initializeCalculator();
         
